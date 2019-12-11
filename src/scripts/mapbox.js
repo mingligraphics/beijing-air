fc3ede0c79918803ed542ef11fa36c28750fe01f
xiaoleiao <yiaoyiyou@gmail.com>
2019-12-11 00:54:16 -0500

    update

--- src/scripts/mapbox.js
+++ src/scripts/mapbox.js
@@ -1,26 +1,26 @@
 import * as d3 from 'd3'
 
 let _map = null
-const _centerLat = 40
-const _centerLng = 116.5
-const _dataFile = require('/data/detector_dots.geojson')
+const _centerLat = 40.05
+const _centerLng = 116.42
+const _dataFile = require('/data/detector_dots_v1.geojson')
 const _accessToken =
   'pk.eyJ1IjoieGlhb2xlaWFvIiwiYSI6ImNqdzc4YTltNzJic3c0OHFxeWhlZDVodGkifQ.fq-KOXgjoJwsGRBsfqmy6w'
 const _mapStyle = 'mapbox://styles/mapbox/light-v10'
 
-const detector = d3.select('.detector')
+// const detector = d3.select('.detector')
 
-const numbers = d3.range(4)
+// const numbers = d3.range(4)
 
-const barChart = detector
-  .selectAll('.group')
-  .data(numbers)
-  .enter()
-  .append('div')
-  .attr('class', 'group')
-  .style('background-color', '#CCCCCC')
+// const barChart = detector
+//   .selectAll('.group')
+//   .data(numbers)
+//   .enter()
+//   .append('div')
+//   .attr('class', 'group')
+//   .style('background-color', '#CCCCCC')
 
-console.log('barchart is', barChart._groups)
+// console.log('barchart is', barChart._groups)
 
 // d3.csv(require('../data/BJ_detector_v2.csv'))
 //   .then(ready)
@@ -59,7 +59,7 @@ function init() {
     source: 'markers-source',
     paint: {
       'circle-color': 'orange',
-      'circle-radius': 4,
+      'circle-radius': 4.5,
       'circle-stroke-width': 0
     }
   })
@@ -77,19 +77,24 @@ function init() {
     popup = new mapboxgl.Popup()
       .setLngLat(_map.unproject(e.point))
       .setHTML(
-        barChart._groups +
-          '<ul>' +
-          '<img src="' +
-          '">' +
-          '<li>Sale price: <b>' +
+        // '<img src="' +
+        //   '">' +
+        '<h3>' +
+          feature.properties.place +
+          '</h3><p>' +
+          'Number of Pollution Days: ' +
+          '</p>' +
+          '<li>2015: <b>' +
+          feature.properties.y15 +
           '</b></li>' +
-          '<li>Address: <b>' +
-          feature.properties.Address +
+          '<li>2016: <b>' +
+          feature.properties.y16 +
           '</b></li>' +
-          '<li>Property type: <b>' +
-          feature.properties.PropType +
+          '<li>2017: <b>' +
+          feature.properties.y17 +
           '</b></li>' +
-          '</ul>'
+          '<li>2018: <b>' +
+          feature.properties.y18
       )
       .addTo(_map)
   })
